975a4d57be26519cae16c7b82a07667da6d66430
AniketLokhande801 <153910692+AniketLokhande801@users.noreply.github.com>
2025-09-22 10:52:57 +0530

    Add files via upload

---
+++ 22 September - Day 1/evenodd.py
@@ -0,0 +1,15 @@
+# even odd
+# def evenodd(num):
+#         if inp % 2 == 0:
+#             print(f"Even no -{inp}")
+#         else:
+#             print(f"Odd no -{inp}")
+# inp =int(input("Enter number: "))
+# evenodd(inp)
+
+
+# print 1 to 5
+# for i in range(6):
+#     print(i)
+
+
